4887bcfe675ba1ba195992ee1dfc9cd145cf7ac4
rtef119 <rtef119@gmail.com>
2023-10-30 17:47:52 +0100

    dz5

---
+++ main.py
@@ -0,0 +1,14 @@
+import colorama
+
+print(colorama.colorama_text())
+print("----")
+print(colorama.Cursor)
+print("----")
+print(colorama.initialise)
+print("----")
+print(colorama.Style)
+
+#print(colorama.executable)
+#print(colorama.version)
+#print(colorama.platform)
+#print(colorama.argv)
